0c5dad431ede9d6fd6bb89e725c7214c1a316143
roheerumi <ohrory218@gmail.com>
2023-04-27 15:18:57 +0900

    add : docker-compose.yml, mongodb.js

--- rora/helloworld-was/routes/example/index.js
+++ rora/helloworld-was/routes/example/index.js
@@ -1,7 +1,19 @@
 'use strict'
+//const mongodb = require('mongodb')
+const { DB_DATABASE } = process.env
 
 module.exports = async function (fastify, opts) {
   fastify.get('/', async function (request, reply) {
-    return 'this is an example'
+    console.log(`[INFO] Mongo DB Connection Try..`)
+    const client = await fastify.mongo.client.connect();
+    try {
+      if (client !== null) {
+        console.log(`[INFO] Mongo DB Connection Success!`)
+        console.log(client)
+        reply.code(200).send(`[INFO] Mongo DB Connection Success!`)
+      }
+    } finally {
+      client.close();
+    }
   })
 }
