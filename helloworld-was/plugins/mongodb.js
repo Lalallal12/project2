b89827f50d87918c2b2caddad5543f74e2dd6f18
roheerumi <ohrory218@gmail.com>
2023-05-02 15:23:24 +0900

    feat : team 9 merge

---
+++ helloworld-was/plugins/mongodb.js
@@ -0,0 +1,13 @@
+const fp = require('fastify-plugin')
+const { MONGO_HOSTNAME, MONGO_USERNAME, MONGO_PASSWORD} = process.env
+
+module.exports = fp(async function (fastify, opts) {
+  
+  fastify.register(require('@fastify/mongodb'), {
+    // force to close the mongodb connection when app stopped
+    // the default value is false
+    forceClose: true,
+    url: `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:27017/?authMechanism=DEFAULT`
+  })  
+})
+
